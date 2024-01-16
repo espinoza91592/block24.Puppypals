d84ecb32a1db82e3a390737af3463ca8f50cf548
espinoza91592 <espinoza91592@gmail.com>
2024-01-15 22:02:51 -0600

    featNewPup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { puppyList } from "./data";
 
-const App = => {
-
+const App = () => {
   const [puppies, setPuppies] = useState(puppyList);
   console.log(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const handleClick = (id) => {
+    console.log("Click Me!");
+    setFeatPupId(id);
+  };
+
+  const featuredPup = puppies.find((pup) => {
+    return pup.id === featPupId;
+  });
+
+  console.log("featured", featuredPup);
 
   return (
     <div>
       <h1>Puppies</h1>
       {puppies.map((puppy) => {
-        return <p>{puppy.name}</p>;
+        return (
+          <p key={puppy.id} onClick={() => handleClick}>
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
-  )
+  );
 };
 
 export default App;
